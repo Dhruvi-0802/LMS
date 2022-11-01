0d6cf38b3ba573f179c0613f2bed104d5bc8677e
Dhruvi-0802 <dpsheth0802@gmail.com>
2022-11-01 22:03:57 +0530

    Dashboard

--- lms_frontend/src/components/Main.js
+++ lms_frontend/src/components/Main.js
@@ -3,6 +3,7 @@ import Home from './Home';
 import CourseDetail from './CourseDetail';
 import Login from './User/Login';
 import Register from './User/Register';
+import Dashboard from './User/Dashboard';
 import About from './About';
 import Footer from './Footer';
 
@@ -18,6 +19,7 @@ function Main(){
             <Route path='/detail/:course_id' element={<CourseDetail />} />
             <Route path='/user-login' element={<Login />} />
             <Route path='/user-register' element={<Register />} />
+            <Route path='/user-dashboard' element={<Dashboard />} />
         </Switch>
         <Footer />
     </div>
