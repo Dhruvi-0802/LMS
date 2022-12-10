e874ee25ff704cfa8d7d5b5dd41257b18dc4464e
Dhruvi-0802 <dpsheth0802@gmail.com>
2022-12-10 15:18:30 +0530

    Rating in teacher dashboard

--- lms_frontend/src/components/Teacher/EnrolledStudents.js
+++ lms_frontend/src/components/Teacher/EnrolledStudents.js
@@ -35,17 +35,17 @@ function EnrolledStudents(){
                                         <th>Name</th>
                                         <th>Email</th>
                                         <th>Username</th>
-                                        <th>Action</th>
+                                        <th>Inrested Categories</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     {StudentData.map((row,index) =>
                                     <tr>
-                                        <td><Link to={`/view-student/`+row.student.id}>{row.student.full_name}</Link></td>
+                                        <td>{row.student.full_name}</td>
                                         <td>{row.student.email}</td>
                                         <td>{row.student.username}</td>
                                         <td>
-                                            <Link class="btn btn-info btn-sm" to={`/view-student/`+row.student.id}>View</Link>   
+                                        {row.student.interested_categories}  
                                         </td>
                                     </tr>
                                     )}
--- lms_frontend/src/components/Teacher/TeacherCourses.js
+++ lms_frontend/src/components/Teacher/TeacherCourses.js
@@ -5,7 +5,6 @@ import axios from 'axios';
 const baseUrl='http://127.0.0.1:8000/api';
 function MyCourses(){
     const [courseData,setCourseData]=useState([]);
-
     const teacherId=localStorage.getItem('teacherId');
     // Fetch courses when page load
     useEffect(()=>{
@@ -41,12 +40,21 @@ function MyCourses(){
                                 <tbody>
                                     {courseData.map((course,index) => 
                                     <tr>
-                                        <td><Link to={`/all-chapters/`+course.id}>{course.title}</Link></td>
+                                        <td>
+                                            <Link to={`/all-chapters/`+course.id}>{course.title}</Link>
+                                            <hr />
+                                            {course.course_rating && 
+                                                <span>Rating: {course.course_rating}/5</span>
+                                            }
+                                            {!course.course_rating && 
+                                                <span>Rating: 0/5</span>
+                                            }
+                                        </td>
                                         <td><img src={course.featured_img} width="80" className='rounded' alt={course.title} /></td>
                                         <td><Link to={`/enrolled-students/`+course.id}>{course.total_enrolled_students}</Link></td>
                                         <td>
-                                            <Link class="btn btn-info btn-sm" to={`/edit-course/`+course.id}>Edit</Link>   
-                                            <Link class="btn btn-success btn-sm ms-2" to={`/add-chapter/`+course.id}>Add Chapter</Link>
+                                            <Link className="btn btn-info btn-sm" to={`/edit-course/`+course.id}>Edit</Link>   
+                                            <Link className="btn btn-success btn-sm ms-2" to={`/add-chapter/`+course.id}>Add Chapter</Link>
                                             <button className='btn btn-danger btn-sm ms-2'>Delete</button>
                                         </td>
                                     </tr>
