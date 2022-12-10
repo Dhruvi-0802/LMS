a70555ea8a16af63dccc28d2523d292821d92847
Dhruvi-0802 <dpsheth0802@gmail.com>
2022-12-10 10:50:16 +0530

    Fix Student login logout

--- lms_api/main/urls.py
+++ lms_api/main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #Teacher
+    # Teacher
     path('teacher/', views.TeacherList.as_view()),
     path('teacher/<int:pk>/', views.TeacherDetail.as_view()),
     path('teacher-login',views.teacher_login),
@@ -12,7 +12,7 @@ urlpatterns = [
     path('course/', views.CourseList.as_view()),
     # Course Detail
     path('course/<int:pk>/', views.CourseDetailView.as_view()),
-    # Specific Course CHapter
+    # Specific Course Chapter
     path('course-chapters/<int:course_id>', views.CourseChapterList.as_view()),
     # Specific Chapter
     path('chapter/<int:pk>', views.ChapterDetailView.as_view()),
@@ -20,7 +20,8 @@ urlpatterns = [
     path('teacher-courses/<int:teacher_id>', views.TeacherCourseList.as_view()),
     # Course Detail
     path('teacher-course-detail/<int:pk>', views.TeacherCourseDetail.as_view()),
-    
+
     # Student
+    path('student/', views.StudentList.as_view()),
     path('student-login',views.student_login),
 ]
